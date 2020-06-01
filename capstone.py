bd80bfa4f0012116707c78b708352d9e35d0ab5e
서귀완 책임/증착/Encap 기술팀/SGW0214 <sgw0214@LGDISPLAY.GLOBAL>
2020-06-01 09:22:05 +0900

    add address

--- capstone.py
+++ capstone.py
@@ -1,4 +1,2 @@
-import pandas as pd
-import numpy as np
-print("Hello Capstone Project Course!")
+https://dataplatform.cloud.ibm.com/analytics/notebooks/v2/79037bd5-4de5-4c4b-a8a4-9cb74fd70aa3/view?access_token=381333acee0585eac6d39b1a79323bc2381052c321502d5d5a8bcd2ad1b1e347
 
